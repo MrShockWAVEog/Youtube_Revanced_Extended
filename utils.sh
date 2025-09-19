82ed0db7a9bcaece63b92f9f247542ae5ab84daf
MD. Jadly Bin Salam Shovon <shovon129131@gmail.com>
2025-09-19 16:57:37 +0600

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -652,7 +652,8 @@ name=${2}
 version=v${3}
 versionCode=${NEXT_VER_CODE}
 author=MrShockWAVEog
-description=${4}" >"${6}/module.prop"
+description=${4}
+banner=https://raw.githubusercontent.com/MrShockWAVEog/Youtube_Revanced_Extended/refs/heads/main/revanced-headline-vertical-dark.png" >"${6}/module.prop"
 
 	if [ "$ENABLE_MAGISK_UPDATE" = true ]; then echo "updateJson=${5}" >>"${6}/module.prop"; fi
 }
