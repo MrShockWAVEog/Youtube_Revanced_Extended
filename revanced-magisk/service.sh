f73d766c600e5383f67109306f69e3ed7fdb08c0
MD. Jadly Bin Salam Shovon <shovon129131@gmail.com>
2025-12-09 11:30:04 +0600

    Add kernel support for zygote adb_data_file

--- revanced-magisk/service.sh
+++ revanced-magisk/service.sh
@@ -47,3 +47,25 @@ run() {
 }
 
 run
+
+if [ "$KSU" = true ] && /data/adb/ksud kernel 2>&1 | grep -q "umount" >/dev/null 2>&1; then
+	echo "allow zygote adb_data_file dir search" > /dev/revanced_rule
+	/data/adb/ksud sepolicy apply /dev/revanced_rule
+	rm /dev/revanced_rule
+
+	APK_PATH=$(pm path "$PKG_NAME")
+	/data/adb/ksud kernel add "$APK_PATH" -f 2 > /dev/null
+	/data/adb/ksud kernel notify-module-mounted > /dev/null
+fi		mp=${line#* } mp=${mp%% *}
+		umount -l "${mp%%\\*}"
+	done
+	if ! chcon u:object_r:apk_data_file:s0 "$RVPATH"; then
+		err "apk not found"
+		return
+	fi
+	mount -o bind "$RVPATH" "$BASEPATH/base.apk"
+	am force-stop "$PKG_NAME"
+	[ -f "$MODDIR/err" ] && mv -f "$MODDIR/err" "$MODDIR/module.prop"
+}
+
+run
